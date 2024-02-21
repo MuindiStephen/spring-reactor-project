8d80794495744272e2b5dc681de42f26c8a1f017
MuindiStephen <stephenmuindi241@gmail.com>
2024-02-21 11:43:05 +0300

    initial reactor

---
+++ src/main/java/com/stevemd/springreactorproject/SpringReactorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stevemd.springreactorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactorProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stevemd/springreactorproject/SpringReactorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevemd.springreactorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactorProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
